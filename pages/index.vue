5e66c91f291261c7d8b5d4114432ab7d3631e551
Yuta <seika0pom@gmail.com>
2018-06-26 23:58:07 +0900

    [edit] コメント修正

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
         <p>転職して1年が経ちました。最近はVue.js、Nuxt.jsにハマっています。</p>
         <p>あとDockerも使ってみたくなったので触ってます。デザインにも興味あります(*'▽'*)
         </p>
-        <p>散歩が好きです。よく歩きます。疲れたら喫茶店に立ち寄りコーヒで一息つくのが大好きです。</p>
+        <p>散歩が好きです。よく歩きます。疲れたら喫茶店に立ち寄りコーヒで一息つくのが大好きです!</p>
         <ul class="content-list">
           <li><i class="fab fa-github fa-lg"></i><a href="https://github.com/neekoko">neekoko</a></li>
           <li><i class="fab fa-twitter-square fa-lg"></i><a href="https://twitter.com/_neekoko">@_neekoko</a></li>
